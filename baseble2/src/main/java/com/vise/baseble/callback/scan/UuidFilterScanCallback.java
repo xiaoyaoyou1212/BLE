0c294f7c8fe23a9c4dd6f875f4414f81c9aabc24
xyy <553252628@qq.com>
2017-09-12 23:30:07 +0800

    增加指定设备UUID扫描过滤规则的扫描回调

---
+++ baseble2/src/main/java/com/vise/baseble/callback/scan/UuidFilterScanCallback.java
@@ -0,0 +1,47 @@
+package com.vise.baseble.callback.scan;
+
+import android.os.ParcelUuid;
+
+import com.vise.baseble.model.BluetoothLeDevice;
+import com.vise.baseble.model.BluetoothLeDeviceStore;
+
+import java.util.UUID;
+
+/**
+ * @Description: 根据指定uuid过滤设备
+ * @author: <a href="http://www.xiaoyaoyou1212.com">DAWI</a>
+ * @date: 17/9/12 23:20.
+ */
+public class UuidFilterScanCallback extends ScanCallback {
+    private UUID uuid;//设备uuid
+
+    public UuidFilterScanCallback(IScanCallback scanCallback) {
+        super(scanCallback);
+    }
+
+    public UuidFilterScanCallback setUuid(String uuid) {
+        this.uuid = UUID.fromString(uuid);
+        bluetoothLeDeviceStore.clear();
+        return this;
+    }
+
+    public UuidFilterScanCallback setUuid(UUID uuid) {
+        this.uuid = uuid;
+        bluetoothLeDeviceStore.clear();
+        return this;
+    }
+
+    @Override
+    public BluetoothLeDeviceStore onFilter(BluetoothLeDevice bluetoothLeDevice) {
+        if (bluetoothLeDevice != null && bluetoothLeDevice.getDevice() != null
+                && bluetoothLeDevice.getDevice().getUuids() != null
+                && bluetoothLeDevice.getDevice().getUuids().length > 0) {
+            for (ParcelUuid parcelUuid : bluetoothLeDevice.getDevice().getUuids()) {
+                if (uuid != null && uuid == parcelUuid.getUuid()) {
+                    bluetoothLeDeviceStore.addDevice(bluetoothLeDevice);
+                }
+            }
+        }
+        return bluetoothLeDeviceStore;
+    }
+}
