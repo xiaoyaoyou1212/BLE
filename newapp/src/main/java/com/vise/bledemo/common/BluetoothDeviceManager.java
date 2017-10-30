d7fa4ea603196ad66d3c78fc5d2cf0425a30e6a3
xyy <553252628@qq.com>
2017-10-30 23:41:05 +0800

    update commit.

--- newapp/src/main/java/com/vise/bledemo/common/BluetoothDeviceManager.java
+++ newapp/src/main/java/com/vise/bledemo/common/BluetoothDeviceManager.java
@@ -167,8 +167,14 @@ public class BluetoothDeviceManager {
         }
         //蓝牙相关配置修改
         ViseBle.config()
-                .setScanTimeout(BleConstant.TIME_FOREVER)
-                .setMaxConnectCount(3);
+                .setScanTimeout(-1)//扫描超时时间，这里设置为永久扫描
+                .setConnectTimeout(10 * 1000)//连接超时时间
+                .setOperateTimeout(5 * 1000)//设置数据操作超时时间
+                .setConnectRetryCount(3)//设置连接失败重试次数
+                .setConnectRetryInterval(1000)//设置连接失败重试间隔时间
+                .setOperateRetryCount(3)//设置数据操作失败重试次数
+                .setOperateRetryInterval(1000)//设置数据操作失败重试间隔时间
+                .setMaxConnectCount(3);//设置最大连接设备数量
         //蓝牙信息初始化，全局唯一，必须在应用初始化时调用
         ViseBle.getInstance().init(context.getApplicationContext());
         mDeviceMirrorPool = ViseBle.getInstance().getDeviceMirrorPool();
